60624ea4359e15b8dd98485691029b8a66dfa611
ketersammy <ketersammy@duck.com>
2022-06-28 17:00:47 +0300

    Restructured the app structure

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,5 +1,6 @@
-from flaskblog import db
 from datetime import datetime
+from flaskblog import db
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -11,6 +12,7 @@ class User(db.Model):
 
     def __repr__(self):
         return f"User('{self.username}','{self.email}', '{self.image_file}')"
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
